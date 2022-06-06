7f134cd68fbb4ea4c2e2291e4e896271663b2e5a
joshhrrsn <josh.harrison@babylonhealth.com>
2022-06-06 18:27:20 +0200

    added trend classification

--- binance_download.py
+++ binance_download.py
@@ -1,38 +1,62 @@
+from audioop import add
 import sys
 import logging
 import os
 import argparse
+from typing import List
 import pandas as pd
-from binance.client import Client
 from datetime import datetime
+from ta import add_all_ta_features
+from ta.utils import dropna
+from binance.client import Client
+from ta_trends import trend_direction
 
+DF_COLUMNS = ['open_ts','open', 'high', 'low', 'close', 'volume','close_ts', 'qav','num_trades']
 
-def write_data(klines, csv_path):
+def prepare_df(df: pd.DataFrame) -> pd.DataFrame:
+    """ Converts raw string to floats and adds all TA indicators to the dataframe.
+    see: https://technical-analysis-library-in-python.readthedocs.io/en/latest/index.html#examples
+    NB for indicators built from historical data there there are nans for the where not enough prior data exists
+    i.e. rows 0-18 are all nans as by default indicators are built from 20 periods.
+
+    """
+    # convert string to floats
+    for column in DF_COLUMNS:
+        df[column] = pd.to_numeric(df[column])
+
+    # remove empty data
+    df = dropna(df)
+    df = add_all_ta_features(df, open='open', high='high', low='low', close='close', volume='volume')
+    return df
+
+def write_data(klines: List, csv_path: str) -> pd.DataFrame:
     _klines = [k[:-3] for k in klines] # Drop some of the columns we're not interested in
     df = pd.DataFrame(_klines)
-    df.columns = ['open_ts','open', 'high', 'low', 'close', 'volume','close_ts', 'qav','num_trades']
+    df.columns = DF_COLUMNS
 
-    df.index = [datetime.fromtimestamp(x/1000.0) for x in df.close_ts]
-    df.index = [datetime.fromtimestamp(x/1000.0) for x in df.open_ts]
+    df = prepare_df(df)
+    df['trend_direction'] = df.apply(trend_direction, axis=1)
+    df.to_csv(csv_path)
 
+def convert_timestamp(df: pd.DataFrame) -> pd.DataFrame:
+    # nb timestamps are unix time, could optionally convert by * 1000, but maybe not necessary?
     for y in [df.open_ts, df.close_ts]:
         for x in y:
             df.index = [datetime.fromtimestamp(x/1000.0)]
-    # nb timestamps are unix time, could optionally convert by * 1000, but maybe not necessary?
-    df.to_csv(csv_path)
+    return df
 
-def main(start_date, end_date, symbol_a, symbol_b, csv_path):
+def main(start_date: str, end_date: str, symbol_a: str, symbol_b: str, csv_path: str) -> None:
     client = Client(os.environ["BINANCE_API"], os.environ["BINANCE_SECRET"])
     klines = client.get_historical_klines(f"{symbol_a}{symbol_b}", client.KLINE_INTERVAL_15MINUTE, start_date, end_date)
     write_data(klines, csv_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Download historical Binance data.")
-    parser.add_argument('start_date', type=str, help="Start date - human readable (see Binance API docs) - default `1 Jan, 2018`" )
-    parser.add_argument('end_date', type=str, help="- human readable (see Binance API docs) default `1 day ago UTC`")
-    parser.add_argument('symbol_a', type=str, help='First ticker in the trade - default BTC')
-    parser.add_argument('symbol_b', type=str, help='Secondary ticker in the trade - default USDC')
-    parser.add_argument('csv_path', type=str, help='Output path/filename for CSV. If None uses the concat of the symbols.')
+    parser.add_argument('--start_date',type=str, help="Start date - human readable (see Binance API docs) - default `1 Jan, 2018`", required=False)
+    parser.add_argument('--end_date', type=str, help="- human readable (see Binance API docs) default `1 day ago UTC`",required=False)
+    parser.add_argument('--symbol_a', type=str, help='First ticker in the trade - default BTC',required=False)
+    parser.add_argument('--symbol_b', type=str, help='Secondary ticker in the trade - default USDC',required=False)
+    parser.add_argument('--csv_path', type=str, help='Output path/filename for CSV. If None uses the concat of the symbols.',required=False)
 
     parser.set_defaults(start_date="1 Jan, 2018", end_date="1 day ago UTC", symbol_a="BTC", symbol_b="USDC")
     args = parser.parse_args()
---
+++ ta_trends.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+"""Used for mapping trend directions to the raw data produced from `binance_download.py`
+This helpers have been seperated into their own module as they are used during the GP
+to get the next window of data.
+"""
+
+def trend_direction(adx_row: pd.Series) -> str:
+    """Use as part of a df.apply to map trend direction from ADX data
+
+    NB that ADX is undirectional (provides strength of trend) between 0-100, hence use in combination with directional indicators.
+    see: https://technical-analysis-library-in-python.readthedocs.io/en/latest/ta.html#trend-indicators and
+    https://www.investopedia.com/articles/trading/07/adx-trend-indicator.asp
+
+    """
+    if adx_row.trend_adx_pos > adx_row.trend_adx_neg:
+        return 'UP'
+    elif adx_row.trend_adx_pos < adx_row.trend_adx_neg:
+        return 'DOWN'
+    else:
+        return 'NOTREND'
+
+def filter_trends(adx_df: pd.DataFrame, direction='UP') -> pd.DataFrame:
+    return adx_df.loc[adx_df['trend_direction'] == direction]
+
+def _filter_trend_strength(adx_df: pd.DataFrame, limit=25, op='ge') -> pd.DataFrame:
+    """Helper to filter out all values >= / <= / > / < or == to the limit"""
+    op = f"__{op}__"
+    comparitor = getattr(pd.Series, op)
+    return adx_df.loc[comparitor(adx_df.trend_adx, limit )]
+
+def get_weak_trends(adx_df: pd.DataFrame) -> pd.DataFrame:
+    return _filter_trend_strength(adx_df, op='lt')
+
+def get_strong_trends(adx_df: pd.DataFrame) -> pd.DataFrame:
+    return _filter_trend_strength(_filter_trend_strength(adx_df, op='ge'), limit=50, op='lt')
+
+def get_vstrong_trends(adx_df: pd.DataFrame) -> pd.DataFrame:
+    return _filter_trend_strength(_filter_trend_strength(adx_df, limit=50, op='ge'),limit=75, op='lt')
+
+def get_exstrong_trends(adx_df: pd.DataFrame) -> pd.DataFrame:
+    return _filter_trend_strength(adx_df, limit=75, op='ge')
+
+def generate_trends(df:pd.DataFrame, periods: int=96):
+    """Yield data from the trend window. NB 96 = one day of 15 minute periods"""
+    yield from df[:periods]
